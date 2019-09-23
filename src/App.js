e2b3ff49d3db0aa8669dc00a7cda5e0dc3be289e
Josue Rodriguez <zhippuden@gmail.com>
2019-09-23 17:59:53 -0600

    init task app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
+// function HelloWorld(props) {
+// 	return (
+// 		<div className='hello'>
+// 			<h3>{props.subtitle}</h3>
+// 			{props.mytext}
+// 		</div>
+// 	);
+// }
+
+class HelloWorld extends Component {
+	state = {
+		show: true
+	};
+
+	///se crea una function para hacer bind en la class
+	toggleShow = () => {
+		this.setState({ show: !this.state.show });
+	};
+
+	///en los componentes por clase se usa un render antes que el return
+	render() {
+		if (this.state.show) {
+			return (
+				<div className='hello'>
+					<h3>{this.props.subtitle}</h3>
+					{this.props.mytext}
+					<br />
+					<br />
+					<button onClick={this.toggleShow}>Ocultar Elemento!</button>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<h1>Elemento Oculto!</h1>
+					<br />
+					<button onClick={this.toggleShow}>Reactivar Elemento!</button>
+				</div>
+			);
+		}
+	}
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			This is my component:
+			<HelloWorld mytext='Hello Josh' subtitle='lorem ipsum' />
+			<HelloWorld mytext='Hola Mundo' subtitle='lorem ipsum' />
+			<HelloWorld mytext='Hello!' subtitle='lorem ipsum' />
+		</div>
+	);
 }
 
 export default App;
